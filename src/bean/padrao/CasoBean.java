1e2ea85aa04c3625bb1e9cffdea92b0beb998240
McFile <desenv@mcfile.com.br>
2015-07-15 18:31:28 -0300

    Refatoração de código para melhor entendimento e JavaDoc

---
+++ src/bean/padrao/CasoBean.java
@@ -0,0 +1,139 @@
+package bean.padrao;
+
+public class CasoBean {
+
+    public static int COD_TIPO_DOC = 236;
+
+    private int codCaso;
+
+    private String nomeCaso;
+
+    private int codCliente;
+
+    private String parte;
+
+    private String numProcesso;
+
+    private String tipoCaso;
+
+    private String tipoAcao;
+
+    private String foro;
+
+    private String vara;
+
+    private String nomeContrato;
+
+    private String partePrincipal;
+
+    public int getCodCaso() {
+
+        return codCaso;
+    }
+
+    public void setCodCaso(int codCaso) {
+
+        this.codCaso = codCaso;
+    }
+
+    public String getNomeCaso() {
+
+        return nomeCaso;
+    }
+
+    public void setNomeCaso(String nomeCaso) {
+
+        this.nomeCaso = nomeCaso;
+    }
+
+    public int getCodCliente() {
+
+        return codCliente;
+    }
+
+    public void setCodCliente(int codCliente) {
+
+        this.codCliente = codCliente;
+    }
+
+    public String getParte() {
+
+        return parte;
+    }
+
+    public void setParte(String parte) {
+
+        this.parte = parte;
+    }
+
+    public String getNumProcesso() {
+
+        return numProcesso;
+    }
+
+    public void setNumProcesso(String numProcesso) {
+
+        this.numProcesso = numProcesso;
+    }
+
+    public String getTipoCaso() {
+
+        return tipoCaso;
+    }
+
+    public void setTipoCaso(String tipoCaso) {
+
+        this.tipoCaso = tipoCaso;
+    }
+
+    public String getTipoAcao() {
+
+        return tipoAcao;
+    }
+
+    public void setTipoAcao(String tipoAcao) {
+
+        this.tipoAcao = tipoAcao;
+    }
+
+    public String getForo() {
+
+        return foro;
+    }
+
+    public void setForo(String foro) {
+
+        this.foro = foro;
+    }
+
+    public String getVara() {
+
+        return vara;
+    }
+
+    public void setVara(String vara) {
+
+        this.vara = vara;
+    }
+
+    public String getNomeContrato() {
+
+        return nomeContrato;
+    }
+
+    public void setNomeContrato(String nomeContrato) {
+
+        this.nomeContrato = nomeContrato;
+    }
+
+    public String getPartePrincipal() {
+
+        return partePrincipal;
+    }
+
+    public void setPartePrincipal(String partePrincipal) {
+
+        this.partePrincipal = partePrincipal;
+    }
+
+}
