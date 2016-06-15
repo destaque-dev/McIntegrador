8360b5024890ea1ad07c5a8cbc3b21a28dc07cf9
Vinicius <Vinicius@note-vinicius.home>
2016-06-15 18:08:13 -0300

    Logica para tentar novamente iniciar o serviço caso dê erro.

--- src/main/IniciaMcIntegrador.java
+++ src/main/IniciaMcIntegrador.java
@@ -1,87 +1,94 @@
 package main;
 
+import bean.Configuracao;
+import exception.IntegradorException;
 import integracao.IntegradorBase;
 import integracao.IntegradorExemplo;
 import util.Funcoes;
 import util.Log;
-import bean.Configuracao;
-import exception.IntegradorException;
 
 public class IniciaMcIntegrador {
 
-	private static boolean stop = false;
+    private static boolean stop = false;
 
-	public static void start(String[] args) {
+    public static void start(String[] args) {
 
-		try {
-			Log.info("Iniciando McIntegrador...");
+        try {
+            Log.info("Iniciando McIntegrador...");
 
-			Configuracao configuracao = Configuracao.getInstance();
+            Configuracao configuracao = Configuracao.getInstance();
 
-			IntegradorBase integrador = montaIntegrador(configuracao);
+            IntegradorBase integrador = montaIntegrador(configuracao);
 
-			while (!isStop()) {
+            while (!isStop()) {
 
-				try {
-					integrador.verificaRegistrosNovos();
-					integrador.verificaRegistrosAtualizados();
-					integrador.verificaRegistrosRemovidos();
-					integrador.verificaRegistrosEspeciais();
+                try {
+                    integrador.verificaRegistrosNovos();
+                    integrador.verificaRegistrosAtualizados();
+                    integrador.verificaRegistrosRemovidos();
+                    integrador.verificaRegistrosEspeciais();
 
-					// Espera um minuto
-					Thread.sleep(60000);
-				} catch (Throwable e) {
-					Funcoes.trataErro(e, integrador);
-					try {
-						Thread.sleep(60000);
-					} catch (InterruptedException e1) {
+                    // Espera um minuto
+                    Thread.sleep(60000);
+                } catch (Exception e) {
+                    Funcoes.trataErro(e, integrador);
+                    try {
+                        Thread.sleep(60000);
+                    } catch (InterruptedException e1) {
 
-					}
+                    }
 
-				}
+                }
 
-			}
-		} catch (Exception e) {
-			Funcoes.trataErro(e, null);
-		}
+            }
+        } catch (Exception e) {
+            Funcoes.trataErro(e, null);
+            try {
+                Thread.sleep(60000);
+            } catch (InterruptedException e1) {
 
-	}
+            }
+            start(args);
+        }
 
-	private static IntegradorBase montaIntegrador(Configuracao configuracao) throws Exception {
-		IntegradorBase integrador = montaIntegrador();
+    }
 
-		if (integrador == null) {
-			throw new IntegradorException("Integrador correto não encontrado, reveja o .properties");
-		}
+    private static IntegradorBase montaIntegrador(Configuracao configuracao) throws Exception {
 
-		integrador.inicializa(configuracao);
+        IntegradorBase integrador = montaIntegrador();
 
-		return integrador;
-	}
+        if (integrador == null) {
+            throw new IntegradorException("Integrador correto não encontrado, reveja o .properties");
+        }
 
-	public static void stop(String[] args) {
+        integrador.inicializa(configuracao);
 
-		Log.info("Parando McIntegrador");
-		stop = true;
-	}
+        return integrador;
+    }
 
-	public static void main(String[] args) {
+    public static void stop(String[] args) {
 
-		if (args == null || args.length == 0 || "start".equals(args[0])) {
-			start(args);
-		} else if ("stop".equals(args[0])) {
-			stop(args);
-		}
-	}
+        Log.info("Parando McIntegrador");
+        stop = true;
+    }
 
-	public static boolean isStop() {
+    public static void main(String[] args) {
 
-		return stop;
-	}
+        if (args == null || args.length == 0 || "start".equals(args[0])) {
+            start(args);
+        } else if ("stop".equals(args[0])) {
+            stop(args);
+        }
+    }
 
-	public static IntegradorBase montaIntegrador() throws Exception {
+    public static boolean isStop() {
+
+        return stop;
+    }
+
+    public static IntegradorBase montaIntegrador() throws Exception {
 
 		return new IntegradorExemplo();
-	}
+    }
 
 }
